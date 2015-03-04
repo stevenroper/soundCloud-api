07374fd5f81ec7065d2b5211aa824acc7db509f0
Steven Roper <steven.m.roper@gmail.com>
2015-03-03 18:49:52 -0700

    Finishing step 4.

--- mainController.js
+++ mainController.js
@@ -2,4 +2,10 @@ var app = angular.module('sounder');
 
 app.controller('MainController', function($scope, soundService) {
 
+	$scope.getUser = function() {
+		soundService.getUser($scope.searchText).then(function(data) {
+			console.log(data.data);
+		});
+	};
+
 });
--- soundService.js
+++ soundService.js
@@ -3,7 +3,7 @@ var app = angular.module('sounder');
 app.service('soundService', function($http) {
 
 	this.getUser = function(username) {
-		$http({
+		return $http({
 			method: 'GET',
 			url: 'http://api.soundcloud.com/users/' + username + '/tracks.json?client_id=bda4ada8694db06efcac9cf97b872b3e'
 		});
