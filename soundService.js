73eeaea06f01ca4e49529241a04f1afd3aeeb00b
Steven Roper <steven.m.roper@gmail.com>
2015-03-03 18:46:21 -0700

    Finishing step 3 and setting up basic API request.

--- soundService.js
+++ soundService.js
@@ -2,4 +2,10 @@ var app = angular.module('sounder');
 
 app.service('soundService', function($http) {
 
+	this.getUser = function(username) {
+		$http({
+			method: 'GET',
+			url: 'http://api.soundcloud.com/users/' + username + '/tracks.json?client_id=bda4ada8694db06efcac9cf97b872b3e'
+		});
+	}
 });
