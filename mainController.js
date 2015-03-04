4f30deb19eca87e4c810b925b53498db1f9f9584
Steven Roper <steven.m.roper@gmail.com>
2015-03-03 19:07:39 -0700

    Finishing step 7.

--- mainController.js
+++ mainController.js
@@ -1,11 +1,17 @@
 var app = angular.module('sounder');
 
-app.controller('MainController', function($scope, soundService) {
+app.controller('MainController', function($scope, soundService, $sce) {
 
 	$scope.getUser = function() {
 		soundService.getUser($scope.searchText).then(function(data) {
-			console.log(data.data);
+			$scope.userData = data.data;
+			$scope.searchText = '';
 		});
 	};
 
+	$scope.play = function(track_url) {
+		SC.oEmbed(track_url, { auto_play: true }, function(oEmbed) {
+      $scope.$apply($scope.player_html = $sce.trustAsHtml(oEmbed.html));
+    });
+	};
 });
